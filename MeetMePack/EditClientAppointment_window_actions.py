7e6a8ed989fbb4483791e1b53a1c39e1fc6a292a
Harry Katsaris <86739581+Harry2gks@users.noreply.github.com>
2024-05-31 12:39:31 +0200

    Delete MeetMePack directory

--- MeetMePack/EditClientAppointment_window_actions.py
+++
@@ -1,7 +0,0 @@
-from MeetMePack.display import *
-
-
-# function No 1
-def edit_client_rantevou(conn, new_window4, code_value, date_value, start_value, during_value):
-    change_appointment_details(conn, appointment_id=code_value.get(), new_date=date_value.get(), new_start_time=start_value.get(), new_duration=during_value.get())
-    new_window4.destroy()
--- MeetMePack/appointment_window_action.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-from tkinter import ttk
-from MeetMePack.database import *
-from MeetMePack.client import *
-from MeetMePack.appointment import *
-from MeetMePack.display import *
